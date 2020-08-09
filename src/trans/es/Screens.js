74c33f4f5124c792244b6325e6aa451b7e54f630
Hector Zurga <hjzurga@gmail.com>
2020-08-09 15:29:54 -0400

    Added trans for messages

---
+++ src/trans/es/Screens.js
@@ -0,0 +1,5 @@
+export default {
+  Login: {
+    loginButton: 'Iniciar sesión',
+  },
+};
---
+++ src/trans/es/index.js
@@ -0,0 +1,9 @@
+import Components from './Components';
+import Screens from './Screens';
+import words from './words';
+
+export default {
+  Components,
+  Screens,
+  words,
+};
