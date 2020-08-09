27c43245d3bf8b6ad96228a206e1cd27e4753d3f
Hector Zurga <hjzurga@gmail.com>
2020-08-09 18:51:53 -0400

    Adding trans and Xhr initialization

--- src/screens/app/App.jsx
+++ src/screens/app/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 import Routes from '../../routes/routes';
+import setLanguage from '../../trans/trans';
+import Xhr from '../../Xhr';
+
+Xhr.initConfigs();
 
 function App() {
+  setLanguage('es');
+
   return (
     <div className="App">
       <Routes />
