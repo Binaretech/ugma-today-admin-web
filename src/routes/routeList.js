5a4cf4dd2e2d092a2aa374d356c750b708ff36e9
Angel Afonso <angelafonso60@gmail.com>
2020-08-18 14:56:10 -0400

    fixed routes

--- src/routes/routeList.js
+++ src/routes/routeList.js
@@ -1,14 +1,8 @@
 import { lazy } from 'react';
 
-const App = lazy(() => import('../screens/app/App'));
 const NotFound = lazy(() => import('../screens/notFound/NotFound'));
 
 export default [
-  {
-    path: '/',
-    exact: true,
-    component: App,
-  },
   {
     path: '*',
     component: NotFound,
