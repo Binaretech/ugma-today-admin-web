3f93d74d38e6346552e11d276d15fde8d1a62290
Angel Afonso <angelafonso60@gmail.com>
2020-09-04 11:57:20 -0400

    Removed console log in index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './screens/app/App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
+import {
+    Provider
+} from 'react-redux';
 import createStore from './redux/store';
-import { snackbarMessage } from './redux/actions/snackbarActions';
-import { trans } from './trans/trans';
+import {
+    snackbarMessage
+} from './redux/actions/snackbarActions';
+import {
+    trans
+} from './trans/trans';
 
 const store = createStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+    <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
-
-console.log(`${process.env.PUBLIC_URL}/service-worker.js`)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
-  onUpdate: () => store.dispatch(snackbarMessage(trans('Components.snackbar.updateMessage'))),
+    onUpdate: () => store.dispatch(snackbarMessage(trans('Components.snackbar.updateMessage'))),
 });
