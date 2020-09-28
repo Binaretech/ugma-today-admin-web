6dcf02492af8ba301f8cb1e5f615b14620a4bf61
Angel Afonso <angelafonso60@gmail.com>
2020-09-27 20:36:37 -0400

    fix: Fixed space-time gap in login and improved matrix

--- src/redux/reducers/sessionReducer.js
+++ src/redux/reducers/sessionReducer.js
@@ -10,7 +10,6 @@ export default function sessionReducer(state = initialState, action) {
         loading: true,
       };
     case sessionActions.LOGIN:
-      localStorage.setItem('utd', JSON.stringify(action.payload?.data));
       return {
         ...state,
         ...action.payload,
--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -14,6 +14,7 @@ import { setLogin } from '../../redux/actions/sessionActions';
 import { useHistory } from 'react-router-dom';
 import paths from '../../routes/paths';
 import { setErrors } from '../../redux/actions/requestActions';
+import { useDataManager } from '../../utils/customHooks';
 
 const inputValues =
   process.env.REACT_APP_ENV === 'local'
@@ -27,34 +28,32 @@ const inputValues =
     };
 
 function Login() {
+
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const [send,] = useXhr({ url: apiEndpoints.login, method: "POST", showErrorSnackbar: true });
   const history = useHistory();
+  const manager = useDataManager(inputValues);
 
   const onSubmit = () => {
     setLoading(true);
 
     send({
       body: {
-        ...inputValues,
+        ...manager.getData(),
       }
     }).then((response) => {
       setLoading(false);
-      dispatch(setLogin(response));
+      dispatch(setLogin(response?.data));
       history.push(paths.home);
     }).catch((response) => {
       setLoading(false);
       dispatch(setErrors(response));
       dispatch(snackbarMessage(
-        response?.data?.message ||
+        response?.message ||
         trans('Components.snackbar.errorMessage')
       ));
     });
-  };
-
-  function onChange({ target: { name, value } }) {
-    inputValues[name] = value;
   };
 
   return (
@@ -66,7 +65,8 @@ function Login() {
             name="username"
             variant="outlined"
             value={inputValues.username}
-            onChange={onChange}
+            setValue={manager.setValue}
+            setError={manager.setError}
           />
           <TextInput
             label={trans('words.password')}
@@ -74,7 +74,8 @@ function Login() {
             type="password"
             variant="outlined"
             value={inputValues.password}
-            onChange={onChange}
+            setValue={manager.setValue}
+            setError={manager.setError}
           />
           <Button variant="contained" onClick={onSubmit}>
             {trans('Screens.Login.loginButton')}
